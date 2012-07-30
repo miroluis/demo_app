498d6d734b987b131df718506842de5e6194e02c
unknown <MiroFr@MiroFr-PC.(none)>
2012-07-30 12:58:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4c4b09034e21738b144979fe1d3831b3b4e04b0317277f9cba50d200344363ca245a360ea5f609da1007e59205b7c1c664daa5e5dca927e1db91c5832af8e089'
